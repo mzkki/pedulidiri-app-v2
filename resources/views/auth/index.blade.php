9bc37da7e42303f81f5ec1e814e8b78ce1f657c9
mzkki <08harismu@gmail.com>
2022-05-17 11:58:37 +0700

    add favicon

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -8,6 +8,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/app.css">
+    <link rel="shortcut icon" href="https://avatars.githubusercontent.com/u/88536261?s=400&u=3cb1ac0c5e2cb2adc6e7f3e1cf20f802f5acbb99&v=4" type="image/x-icon">
 
     <title>Peduli Diri | {{ $title }}</title>
   </head>

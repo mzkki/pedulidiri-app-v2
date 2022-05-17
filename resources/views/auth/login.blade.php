018bbe7a6941220f283fc0352e657be64e15b076
mzkki <08harismu@gmail.com>
2022-05-17 09:44:00 +0700

    create login and register view also controller

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,35 @@
+@extends('auth.index')
+
+@section('auth')
+<div class="position-absolute top-50 start-50 translate-middle">
+    <form action="/register" method="POST">
+        @csrf
+        <div class="form-floating mb-3">
+            <input type="number" class="form-control @error('nik') is-invalid @enderror formlogin" id="floatingInput" placeholder="28282828" name="nik">
+            <label for="floatingInput">NIK</label>
+            @error('nik')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+        <div class="form-floating">
+            <input type="text" class="form-control @error('fullname') is-invalid @enderror formlogin" id="floatingPassword" placeholder="text" name="fullname">
+            <label for="floatingPassword">Nama Lengkap</label>
+            @error('fullname')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+        <div class="row mt-3">
+            <div class="col">
+                <a href="/login" class="btn">Sudah Punya Akun</a>
+            </div>
+            <div class="col text-end">
+                <button class="btn" type="submit">Daftar</button>
+            </div>
+        </div>
+    </form>
+</div>
+@endsection
